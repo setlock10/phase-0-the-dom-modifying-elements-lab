c75771538169075e8d8b3fa03417df70138e40f1
John J Setlock II <johnsetlock@gmail.com>
2022-05-02 07:58:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const elementMain=document.getElementById("main");
+elementMain.remove();
+const newHeader=document.createElement('h1');
+newHeader.node='h1#victory';
+newHeader.id='victory';
+newHeader.textContent="Jay is the champion";
+document.body.append(newHeader);
+
